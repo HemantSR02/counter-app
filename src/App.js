a84c1f765938f7fe85081965662b157babe86c39
Hemant Devda <hemant.devda02@gmail.com>
2024-02-22 18:26:44 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+
+  const [Count, setCount] = useState(0);
+
+  function up() {
+    if (Count >= 20) {
+      alert("No more Clicks")
+      setCount(20)
+    }
+    else
+      setCount(Count + 1);
+  }
+
+  function down() {
+    if (Count <= 0) {
+      alert("we don't want negatives")
+      setCount(0)
+    }
+    else
+      setCount(Count - 1);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='Counter'>
+        <button className='first'onClick={up}>CountUp{Count}</button>
+        <br></br>
+        <br></br>
+        <button className='first' onClick={down}>CountDown{Count}</button>
+      </div>
     </div>
+
+
   );
 }
 
